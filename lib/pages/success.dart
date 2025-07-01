4cde835fa6eec1fc5f47d189c6224a0be0072610
TaigaTi <tarikabirch@gmail.com>
2025-06-30 20:52:29 -0400

    Created success page

--- lib/pages/success.dart
+++ lib/pages/success.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:re_source/pages/home.dart';
 import 'package:re_source/widgets/custom_appbar.dart';
 import 'package:re_source/widgets/custom_drawer.dart';
 
@@ -11,7 +12,104 @@ class Success extends StatelessWidget {
       backgroundColor: Colors.white,
       appBar: CustomAppBar(),
       drawer: CustomDrawer(),
-      body: Container(),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 35.0,
+              vertical: 75.0,
+            ),
+            child: Column(
+              children: [
+                Card(
+                  color: const Color.fromARGB(255, 233, 233, 233),
+                  elevation: 2.0,
+                  child: Padding(
+                    padding: const EdgeInsets.all(30.0),
+                    child: Column(
+                      spacing: 15,
+                      children: [
+                        Center(
+                          child: Text(
+                            "Success!",
+                            style: TextStyle(
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold,
+                              color: const Color.fromARGB(255, 78, 173, 162),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Color.fromARGB(255, 255, 252, 241),
+                          ),
+                          clipBehavior: Clip.antiAlias,
+                          child: Image(
+                            height: 300,
+                            image: AssetImage("assets/images/success.png"),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 0,
+                            horizontal: 50.0,
+                          ),
+                          child: Center(
+                            child: Text(
+                              "Your resource has been created successfuly!",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: FilledButton(
+                    onPressed: () => {
+                      Navigator.push(
+                        context,
+                        PageRouteBuilder(
+                          pageBuilder:
+                              (context, animation, secondaryAnimation) =>
+                                  const Home(),
+                          transitionDuration: Duration.zero,
+                          reverseTransitionDuration: Duration.zero,
+                        ),
+                      ),
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStateProperty.all(
+                        const Color.fromARGB(255, 87, 175, 161),
+                      ),
+                      minimumSize: WidgetStateProperty.all(
+                        const Size(double.infinity, 40),
+                      ),
+                      shape: WidgetStateProperty.all(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                    child: const Text(
+                      "Back to Home",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:re_source/pages/home.dart';
 import 'package:re_source/pages/library.dart';
+import 'package:re_source/pages/success.dart';
 
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({super.key});
@@ -38,6 +39,20 @@ class CustomDrawer extends StatelessWidget {
                 PageRouteBuilder(
                   pageBuilder: (context, animation, secondaryAnimation) =>
                       const Library(),
+                  transitionDuration: Duration.zero, // 👈 No animation
+                  reverseTransitionDuration: Duration.zero,
+                ),
+              ),
+            },
+          ),
+          ListTile(
+            title: const Text('Success'),
+            onTap: () => {
+              Navigator.push(
+                context,
+                PageRouteBuilder(
+                  pageBuilder: (context, animation, secondaryAnimation) =>
+                      const Success(),
                   transitionDuration: Duration.zero, // 👈 No animation
                   reverseTransitionDuration: Duration.zero,
                 ),
