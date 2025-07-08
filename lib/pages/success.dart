fb9f09600b002c35860adcec69182273b6d15913
TaigaTi <tarikabirch@gmail.com>
2025-07-08 07:17:53 -0400

    Added new resource button to success page

--- lib/pages/success.dart
+++ lib/pages/success.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:re_source/pages/home.dart';
+import 'package:re_source/pages/new_resource.dart';
 import 'package:re_source/widgets/custom_appbar.dart';
 import 'package:re_source/widgets/custom_drawer.dart';
 
@@ -102,6 +103,40 @@ class SuccessPage extends StatelessWidget {
                     child: const Text(
                       "Back to Home",
                       style: TextStyle(fontSize: 16),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: FilledButton(
+                    onPressed: () => {
+                      Navigator.push(
+                        context,
+                        PageRouteBuilder(
+                          pageBuilder:
+                              (context, animation, secondaryAnimation) =>
+                                  const NewResource(),
+                          transitionDuration: Duration.zero,
+                          reverseTransitionDuration: Duration.zero,
+                        ),
+                      ),
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStateProperty.all(
+                       Color.fromARGB(255, 233, 233, 233),
+                      ),
+                      minimumSize: WidgetStateProperty.all(
+                        const Size(double.infinity, 40),
+                      ),
+                      shape: WidgetStateProperty.all(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                    child: const Text(
+                      "Add Another Resource",
+                      style: TextStyle(fontSize: 16, color: Color.fromARGB(255, 110, 110, 110)),
                     ),
                   ),
                 ),
